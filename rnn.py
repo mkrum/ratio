167efdf22b8dcb58366cd43f4c023ac45355fbda
mkrum <mrkrumdick@gmail.com>
2017-12-14 19:03:57 -0500

    2 layers of 100, for consistency

--- rnn.py
+++ rnn.py
@@ -6,7 +6,7 @@ class RNN(LSTM):
     def __init__(self, input_dimension):
 
         NUM_LAYERS = 2
-        HIDDEN_DIM = 256
+        HIDDEN_DIM = 100
         #FLAT_HIDDEN = 64
 
         INPUT_DIM = input_dimension
